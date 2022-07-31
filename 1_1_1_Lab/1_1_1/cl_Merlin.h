e2e50e45f271d36d8ce17d9b80444181e3997ad7
mrNRaven-hub <nikita.volkov0505@mail.ru>
2022-07-31 16:21:06 +0300

    Added 1_1_1_Lab Mirea

---
+++ 1_1_1_Lab/1_1_1/cl_Merlin.h
@@ -0,0 +1,14 @@
+#ifndef CL_merlin_H
+#define CL_merlin_H
+#include "cl_base.h"
+#include <string>
+using namespace std;
+
+class cl_application : public cl_base
+{
+public:
+	cl_application(cl_base* parent);
+	void bild_tree_objects();
+	int exec_app();
+};
+#endif
