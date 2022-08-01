f6b16e81ddf3fe96720a5df916da04e97af44eba
mrNRaven-hub <nikita.volkov0505@mail.ru>
2022-08-01 21:57:42 +0300

    Added 4.1_Lab

---
+++ 4.1_Lab/4.1/4.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	std::ofstream output_current_file{ "Test.txt" };
+	int sum{}, part{};
+
+	output_current_file << "1 2 3 4 5 6 7 8 9 1";
+	output_current_file.close();
+
+	std::ifstream input_current_file;
+	input_current_file.open("Test.txt");
+
+	while (input_current_file >> part)
+		sum += part;
+
+	std::cout << "Сумма чисел равна: " << sum;
+}
