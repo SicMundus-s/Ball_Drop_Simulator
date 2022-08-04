5b1b57ed9674fb01b7ca7d52220368b6c5da5d94
mrNRaven-hub <nikita.volkov0505@mail.ru>
2022-08-04 17:28:24 +0300

    added 4.2_Lab

---
+++ 4.2_Lab/4.2/4.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int search_sign(int user_value)
+{
+	if (user_value > 0)
+		return 1;
+
+	if (user_value < 0)
+		return -1;
+
+	if (user_value == 0)
+		return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int user_value{};
+
+	std::cout << "Введите значениe, чтобы определить его знак: ";
+	std::cin >> user_value;
+
+	std::cout << "Знак вашего числа равен: " << search_sign(user_value);
+
+	return 0;
+}
